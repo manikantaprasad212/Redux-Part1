b582baa75683f71b8f72239e4ec724059d0b6e71
manikantaprasad212 <manikantaprasadprasadula@gmail.com>
2024-09-25 14:10:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,36 +42,3 @@ reportWebVitals();
 
 
 
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import { createStore } from 'redux';
-// import { Provider } from 'react-redux';
-
-// let store = {
-//   fruits:[],
-//   vegetables:[],
-//   curries:[]
-// }
-
-// let reducer = (latestStore = store,dispatchedObj)=>{
-//   console.log("Inside reducer function");
-//   console.log(dispatchedObj);
-// return latestStore;
-// }
-
-// let Store = createStore(reducer);
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <Provider Store={Store} >
-//     <App />
-//   </Provider>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
